8cb6b0c63e0cd2aa2484d0e6167214702e9fc933
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2025-09-01 09:12:02 -0300

    Add files via upload

---
+++ Tarefa/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+
+class Tarefa(models.Model):
+    OPCOES_STATUS = (
+        ('concluído', 'Concluído'),
+        ('pendente', 'Pendente'),
+        ('adiado', 'Adiado'),
+    )
+    
+    OPCOES_CATEGORIA = (
+        ('urgente', 'Urgente'),
+        ('importante', 'Importante'),
+        ('precisa ser feito', 'Precisa ser feito'),
+    )
+    
+    
+    descricao = models.CharField(max_length=400)
+    criacao = models.DateTimeField(auto_now_add=True)
+    categoria = models.CharField(max_length=25, choices=OPCOES_CATEGORIA,
+                                 default='importante')
+    status = models.CharField(max_length=25, choices=OPCOES_STATUS, 
+                              default='pendente')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#create youe models here.]
+# descrção
+# data de criação
+# categoria / urgente, importante ou precisa ser feito /
+# status / concluido ou pedente ou adiado ?
